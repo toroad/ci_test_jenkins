f211671b84580d2b6190916fa24e4d5f0476494e
空文 <jincai.zjc@alibaba-inc.com>
2021-05-06 16:32:52 +0800

    增加测试脚本

---
+++ cases/test1.py
@@ -0,0 +1,25 @@
+# content of test_sample.py
+def inc(x):
+    return x + 2
+
+
+def test_answer1():
+    assert inc(3) == 5
+
+def test_answer2():
+    assert inc(3) == 5
+
+def test_answer3():
+    assert inc(3) == 5
+
+def test_answer4():
+    assert inc(3) == 5
+
+def test_answer5():
+    assert inc(3) == 5
+
+def test_answer6():
+    assert inc(3) == 5
+
+def test_answer7():
+    assert inc(3) == 5
